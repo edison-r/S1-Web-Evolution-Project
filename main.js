053343b6a5034081fc52e36890e17736ab3949a5
Edison <ronquillo5@hotmail.com>
2025-04-30 00:07:03 +0200

    buttons and variables refactored in sass, minor changes in the html, .js added to articles menu

--- main.js
+++ main.js
@@ -14,3 +14,32 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     );
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tab1 = document.getElementById("tab1");
+    const tab2 = document.getElementById("tab2");
+    const tab3 = document.getElementById("tab3");
+
+    const article1 = document.getElementById("article1");
+    const article2 = document.getElementById("article2");
+    const article3 = document.getElementById("article3");
+
+    tab1.addEventListener("click", function () {
+        article1.classList.add("active");
+        article2.classList.remove("active");
+        article3.classList.remove("active");
+    }
+    );
+    tab2.addEventListener("click", function () {
+        article1.classList.remove("active");
+        article2.classList.add("active");
+        article3.classList.remove("active");
+    }
+    );
+    tab3.addEventListener("click", function () {
+        article1.classList.remove("active");
+        article2.classList.remove("active");
+        article3.classList.add("active");
+    }
+    );
+});
